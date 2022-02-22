977ec83bb0dd2441837df8bb1f8b1689411337fe
Stefania Popa <stefania_florina10@yahoo.com>
2022-02-22 11:05:38 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => {
+  return <h1 title='This works!'>Hi, this is ReactJS!</h1>; //translate to => React.createElement('h1', {title:'This Works!'}, 'Hi, this is ReactJS!')
+};
+
+export default App;
