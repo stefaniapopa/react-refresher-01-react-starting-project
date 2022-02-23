8d21f398c51f21b7f33bce1aa8c33ea0d6520df8
Stefania Popa <stefania_florina10@yahoo.com>
2022-02-23 11:45:09 +0200

    fetch using input

--- src/components/NewGoal/NewGoal.js
+++ src/components/NewGoal/NewGoal.js
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, {useState} from "react";
 
-import './NewGoal.css';
+import "./NewGoal.css";
 
-const NewGoal = props => {
-  const addGoalHandler = event => {
+const NewGoal = (props) => {
+  const [enteredText, setenteredText] = useState('')
+
+  const addGoalHandler = (event) => {
     event.preventDefault();
 
     const newGoal = {
       id: Math.random().toString(),
-      text: 'My new goal!'
+      text: enteredText,
     };
+
+    setenteredText('');
 
     props.onAddGoal(newGoal);
   };
 
+  const textChangeHandler = event => {
+      setenteredText(event.target.value);
+  }
+
   return (
     <form className="new-goal" onSubmit={addGoalHandler}>
-      <input type="text" />
+      <input type="text" value={enteredText} onChange={textChangeHandler} />
       <button type="submit">Add Goal</button>
     </form>
   );
